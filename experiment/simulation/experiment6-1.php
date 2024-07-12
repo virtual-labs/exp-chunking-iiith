ea8863daa2c41bfbf37959fabb6dd4c362baf6bd
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-07-12 16:12:48 +0530

    Upgraded PHP version

--- experiment/simulation/experiment6-1.php
+++ experiment/simulation/experiment6-1.php
@@ -83,7 +83,7 @@ function clearTable()
 <?php 
      
       $lang_opt=$_GET['lang'];
-      if(strcmp($lang_opt, "1")==0)
+      if(strcmp((string) $lang_opt, "1")==0)
       { $sentence_path="Exp6/Eng-Sen.txt";
         $option_path="Exp6/answer_opt_eng.txt";
       }
@@ -93,25 +93,27 @@ function clearTable()
       }
       $f1 =  fopen($sentence_path, "r");
       $buffer=fread($f1, filesize($sentence_path));
-      $options=split("\n", $buffer); 
+      $options=preg_split('#
+#m', $buffer); 
       $sel_sen=$options[$_GET['ind']];
-      $sen=split("&",$sel_sen);      
+      $sen=preg_split('#&#m',(string) $sel_sen);      
       $question=$sen[0];
       $answer=$sen[1];      
-      $answer_chunk=split(" ", $answer);
+      $answer_chunk=preg_split('# #m', (string) $answer);
       $f2 =  fopen($option_path, "r");
       $buffer2=fread($f2, filesize($option_path));
-      $ans_tokens=split("\n", $buffer2); 
+      $ans_tokens=preg_split('#
+#m', $buffer2); 
       
  ?>
-      <form action="javascript:checkAnswer()" target="_parent" method="post">
+      <form action="javascript:checkAnswer()" target="_self" method="post">
       <table cellspacing="-2" cellpadding="4" border="1" style="text-align:center;">
       <?php
       echo "<input type=\"hidden\" name=\"sentence_info\" id=\"sentence_info\" value=\"".$answer."\"/>";
       echo "<th>Lexicon</th><th>POS</th><th>Chunk</th><th id=\"cor_head\"></th><th id=\"ans_head\"></th>"; 
       for($j=0;$j<sizeof($answer_chunk);$j++){ 
       echo "<tr>";
-      $part_answer=split("/",$answer_chunk[$j]);
+      $part_answer=preg_split('#\/#m',(string) $answer_chunk[$j]);
       echo "<td>$part_answer[0]</td><td> $part_answer[1]</td><td><select name=\"t\" id=\"token".$j."\">";
       for($y=0; $y<count($ans_tokens)-1; $y++)
             echo "<option>".$ans_tokens[$y]."</option>";
